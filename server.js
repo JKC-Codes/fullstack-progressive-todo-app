caab184dedf7dd0514a14784417046e6a9047d9d
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2021-04-05 19:13:40 +0100

    Add post requests

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import mongodb from 'mongodb';
 dotenv.config();
 
 const app = express();
-const router = express.Router();
+const todoRoutes = express.Router();
 const port = process.env.PORT;
 const connectionString = process.env.MONGODB_URI;
 const MongoClient = new mongodb.MongoClient(connectionString, {
@@ -14,6 +14,12 @@ const MongoClient = new mongodb.MongoClient(connectionString, {
   useUnifiedTopology: true,
 });
 
+todoRoutes.use(cors());
+todoRoutes.use(express.json());
+todoRoutes.use(express.static('public'));
+todoRoutes.use(express.urlencoded({extended: true}));
+
+app.use(todoRoutes);
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
@@ -25,25 +31,39 @@ app.listen(port, function() {
 	console.log(`Server listening on port ${port}`);
 });
 
-try {
-	await MongoClient.connect();
-	const todoCollection = await MongoClient.db().collection('todos');
-	console.log('Connected to todos collection');
+async function start() {
+	let todoCollection;
 
-	app.get('/', (req, res) => {
-		res.render('index.ejs', {todos: [{uid: 123, text: 'test', completed: true}]});
+	try {
+		await MongoClient.connect();
+		todoCollection = await MongoClient.db().collection('todos');
+		console.log('Connected to todos collection');
+	}
+	catch(err) {
+		throw new Error(err);
+	}
+
+	app.get('/', async (req, res) => {
+		const todos = await todoCollection.find().sort({text: 1}).toArray();
+		res.render('index.ejs', {todos: todos});
 	});
 
-	router.route('/api/todos')
-	.post((req, res) => {
-
+	todoRoutes.route('/api/todos')
+	.post(async (req, res) => {
+		if(req.body.text) {
+			await todoCollection.insertOne({
+				text: req.body.text,
+				complete: false
+			});
+		}
+		res.redirect('/');
 	})
 	.patch((req, res) => {})
 	.delete((req, res) => {})
 }
-catch(err) {
-	throw new Error(err);
-}
-finally {
-	await MongoClient.close();
-}
+
+start()
+.catch(err => {
+	console.error(err);
+})
+.finally(MongoClient.close());
